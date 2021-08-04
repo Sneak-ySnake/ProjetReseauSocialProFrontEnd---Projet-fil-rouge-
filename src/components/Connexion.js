91df6290f9d5569cacd89117751a8cd20af6068d
Théo <theomaes54@gmail.com>
2021-08-04 11:44:40 +0200

    Update Connexion.js

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -46,7 +46,7 @@ class Connexion extends React.Component {
     };
 
     tempsSession = () => {
-        var temps = Date.now() + 10000;
+        var temps = Date.now() + 36000000;
         localStorage.setItem("tempsSession", temps);
     }
 
@@ -72,26 +72,6 @@ class Connexion extends React.Component {
         );
     };
 
-/*
-    render() {
-        return(
-            <div className="Connexion">
-                <form action="/PROJET_FIL_ROUGE_tender_du_poulet/login" method="post">
-                    <br/>
-                    Email :
-                    <br/>
-                    <input type="text" name="email"></input>
-                    <br/>
-                    Mot de passe :
-                    <br/>
-                    <input type="password" name="mdp"></input>
-                    <br/>
-                    <input class="btn btn-primary" type="submit"></input>
-                </form>
-            </div>
-        );
-    };
-*/
 }
 
 
