355bc0e78cd8eac2f478f60905a95c13b56654f9
Théo <theomaes54@gmail.com>
2021-08-05 12:33:38 +0200

    Update Profil.js

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -8,9 +8,63 @@ class Profil extends React.Component {
     super();
 
     this.state = {
-      utilisateur: JSON.parse(localStorage.getItem("utilisateur"))
+      id_utilisateur: "",
+      nom_utilisateur: "",
+      prenom_utilisateur: "",
+      site_web: "",
+      telephone: "",
+      poste_occupe: "",
+      email_utilisateur: "",
+      siret: "",
+      nom_entreprise: "",
+      num_voie: "",
+      adresse: "",
+      domaine: {},
+      ville: {}
     };
   }
+
+  componentDidMount() {
+    this.setState({
+      id_utilisateur: JSON.parse(localStorage.getItem("utilisateur")).id_utilisateur,
+      nom_utilisateur: JSON.parse(localStorage.getItem("utilisateur")).nom_utilisateur,
+      prenom_utilisateur: JSON.parse(localStorage.getItem("utilisateur")).prenom_utilisateur,
+      site_web: JSON.parse(localStorage.getItem("utilisateur")).site_web,
+      telephone: JSON.parse(localStorage.getItem("utilisateur")).telephone,
+      poste_occupe: JSON.parse(localStorage.getItem("utilisateur")).poste_occupe,
+      email_utilisateur: JSON.parse(localStorage.getItem("utilisateur")).email_utilisateur,
+      siret: JSON.parse(localStorage.getItem("utilisateur")).siret,
+      nom_entreprise: JSON.parse(localStorage.getItem("utilisateur")).nom_entreprise,
+      num_voie: JSON.parse(localStorage.getItem("utilisateur")).num_voie,
+      adresse: JSON.parse(localStorage.getItem("utilisateur")).adresse,
+      domaine: JSON.parse(localStorage.getItem("utilisateur")).domaine,
+      ville: JSON.parse(localStorage.getItem("utilisateur")).ville
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  };
+
+  modifProfil = (e) => {
+    e.preventDefault();
+    axios.post("/PROJET_FIL_ROUGE_tender_du_poulet/updateUtilisateur", {
+      id_utilisateur: this.state.id_utilisateur,
+      nom_utilisateur: this.state.nom_utilisateur,
+      prenom_utilisateur: this.state.prenom_utilisateur,
+      site_web: this.state.site_web,
+      telephone: this.state.telephone,
+      poste_occupe: this.state.poste_occupe,
+      email_utilisateur: this.state.email_utilisateur,
+      siret: this.state.siret,
+      nom_entreprise: this.state.nom_entreprise,
+      num_voie: this.state.num_voie,
+      adresse: this.state.adresse,
+      domaine: this.state.domaine,
+      ville: this.state.ville,
+      mot_de_passe_utilisateur: "test"
+    });
+  };
 
   render() {
     {
@@ -20,41 +74,46 @@ class Profil extends React.Component {
     if (u == null) {
       return <Redirect to="/" />;
     }
-    if (Date.now() > tempsSession) {
+    else if (Date.now() > tempsSession) {
       localStorage.clear();
       return <Redirect to="/" />;
     }
     else {
-      var temps = Date.now() + 3600*1000;
+      var temps = Date.now() + 3600 * 1000;
       localStorage.setItem("tempsSession", temps);
     }
     return (
       <div className="Profil">
+       
+        <form onSubmit={this.modifProfil}>
+          <br />
+          Modif : <br /><br />
+          <input type="submit"></input> <br/>
 
-        Nom : <br />
-        {this.state.utilisateur.nom_utilisateur} <br /><br />
-        Prenom : <br />
-        {this.state.utilisateur.prenom_utilisateur} <br /><br />
-        Site Web : <br />
-        {this.state.utilisateur.site_web} <br /><br />
-        Téléphone : <br />
-        {this.state.utilisateur.telephone} <br /><br />
-        Poste occupé : <br />
-        {this.state.utilisateur.poste_occupe} <br /><br />
-        Email : <br />
-        {this.state.utilisateur.email_utilisateur} <br /><br />
-        Siret : <br />
-        {this.state.utilisateur.siret} <br /><br />
-        Entreprise : <br />
-        {this.state.utilisateur.nom_entreprise} <br /><br />
-        Adresse : <br />
-        {this.state.utilisateur.num_voie} {this.state.utilisateur.adresse} <br /><br />
-        Ville : <br />
-        {this.state.utilisateur.ville.nom_ville} <br /><br />
-        Pays : <br />
-        {this.state.utilisateur.ville.pays.nom_pays} <br /><br />
-        Domaine : <br />
-        {this.state.utilisateur.domaine.nom_domaine}<br /><br />
+          Nom : <br />
+          <input type="text" value={this.state.nom_utilisateur} name="nom_utilisateur" onChange={this.handleChange}></input> <br /><br />
+          Prenom : <br />
+          <input type="text" value={this.state.prenom_utilisateur} name="prenom_utilisateur" onChange={this.handleChange}></input> <br /><br />
+          Site Web : <br />
+          <input type="text" value={this.state.site_web} name="site_web" onChange={this.handleChange}></input> <br /><br />
+          Téléphone : <br />
+          <input type="text" value={this.state.telephone} name="telephone" onChange={this.handleChange}></input> <br /><br />
+          Poste occupé : <br />
+          <input type="text" value={this.state.poste_occupe} name="poste_occupe" onChange={this.handleChange}></input> <br /><br />
+          Email : <br />
+          <input type="text" value={this.state.email_utilisateur} name="email_utilisateur" onChange={this.handleChange}></input> <br /><br />
+          Siret : <br />
+          <input type="text" value={this.state.siret} name="siret" onChange={this.handleChange}></input> <br /><br />
+          Entreprise : <br />
+          <input type="text" value={this.state.nom_entreprise} name="nom_entreprise" onChange={this.handleChange}></input> <br /><br />
+          Adresse : <br />
+          <input type="text" value={this.state.adresse} name="adresse" onChange={this.handleChange}></input> <br /><br />
+          Ville : <br />
+          {this.state.ville.nom_ville} <br /><br />
+          Domaine : <br />
+          {this.state.domaine.nom_domaine}<br /><br />
+          
+        </form>
 
       </div>
     );
